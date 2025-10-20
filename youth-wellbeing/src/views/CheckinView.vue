7736cfc7943368b35d9a8a2b9367e9ed17dddd9a
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:17:06 +1100

    submit function of moodcheck

--- youth-wellbeing/src/views/CheckinView.vue
+++ youth-wellbeing/src/views/CheckinView.vue
@@ -9,3 +9,10 @@ const mood = vref(3); const note = vref(''); const uid = vref<string|null>(null)
 const router = useRouter()
 onAuthStateChanged(auth, (u)=> uid.value = u?.uid ?? null)
 
+const submit = async () => {
+  if (!uid.value) return router.push('/signin')
+  await addDoc(collection(db,'checkins'), {
+    uid: uid.value, mood: Number(mood.value), note: note.value, createdAt: serverTimestamp()
+  })
+  note.value = ''; mood.value = 3; alert('Saved!')
+}
