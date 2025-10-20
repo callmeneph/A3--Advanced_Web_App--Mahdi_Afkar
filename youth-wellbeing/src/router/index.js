11d66795a255d3eb27c2fa9926e72b5866a930ed
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:06:52 +1100

    importing the tabs i wanna show on my website

--- youth-wellbeing/src/router/index.js
+++ youth-wellbeing/src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+const HomeView = () => import('@/views/HomeView.vue')
+const ResourcesView = () => import('@/views/ResourcesView.vue')
+const CheckinView = () => import('@/views/CheckinView.vue')
+const MyCheckinsView = () => import('@/views/MyCheckinsView.vue')
+const AdminView = () => import('@/views/AdminView.vue')
+const SignInView = () => import('@/views/SignInView.vue')
 
-export default router
+
+
