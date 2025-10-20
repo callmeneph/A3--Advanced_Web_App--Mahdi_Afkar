ef7a2a6840d8bd3225851d44b47afd9d223aeb78
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:12:52 +1100

    adding all the router links to app.vue

--- youth-wellbeing/src/App.vue
+++ youth-wellbeing/src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav class="navbar navbar-light bg-light px-3">
+    <RouterLink to="/" class="navbar-brand">Wellbeing</RouterLink>
+    <div class="d-flex gap-3">
+      <RouterLink to="/resources">Resources</RouterLink>
+      <RouterLink to="/checkin">Check-in</RouterLink>
+      <RouterLink to="/me/checkins">My History</RouterLink>
+      <RouterLink to="/admin">Admin</RouterLink>
+      <RouterLink to="/signin">Sign in</RouterLink>
+    </div>
+  </nav>
+  <RouterView />
 </template>
 
-<style scoped></style>
+<style>
+a { text-decoration:none }
+.navbar a.router-link-exact-active { font-weight:700 }
+</style>
