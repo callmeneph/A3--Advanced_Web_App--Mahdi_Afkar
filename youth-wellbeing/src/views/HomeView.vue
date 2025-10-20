1221b36bc049fa83e8d5186ef7344b99cabb641d
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:08:30 +1100

    adding homepage tab on view

---
+++ youth-wellbeing/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container py-4">
+    <h1>Youth Mental Health & Wellbeing</h1>
+    <p class="mt-2">Explore resources and record your daily mood.</p>
+    <RouterLink to="/resources" class="btn btn-primary me-2">Resources</RouterLink>
+    <RouterLink to="/checkin" class="btn btn-success me-2">Mood Check-in</RouterLink>
+    <RouterLink to="/me/checkins" class="btn btn-outline-secondary">My History</RouterLink>
+  </div>
+</template>
