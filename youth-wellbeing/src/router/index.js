abdd65d3f49724aff6bcda5db9da788a836bd0d7
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 14:17:56 +1100

    it runs but its ugly lets fix it

--- youth-wellbeing/src/router/index.js
+++ youth-wellbeing/src/router/index.js
@@ -1,24 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const HomeView = () => import('@/views/HomeView.vue')
-const ResourcesView = () => import('@/views/ResourcesView.vue')
-const CheckinView = () => import('@/views/CheckinView.vue')
-const MyCheckinsView = () => import('@/views/MyCheckinsView.vue')
-const AdminView = () => import('@/views/AdminView.vue')
-const SignInView = () => import('@/views/SignInView.vue')
-
-
+const HomeView        = () => import('@/views/HomeView.vue')
+const CheckinView     = () => import('@/views/CheckinView.vue')
+const MyCheckinsView  = () => import('@/views/MyCheckinsView.vue')
+const ResourcesView   = () => import('@/views/ResourcesView.vue')
+const SignInView      = () => import('@/views/SignInView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/resources', name: 'resources', component: ResourcesView },
-    { path: '/checkin', name: 'checkin', component: CheckinView },
-    { path: '/me/checkins', name: 'my-checkins', component: MyCheckinsView },
-    { path: '/admin', name: 'admin', component: AdminView },
-    { path: '/signin', name: 'signin', component: SignInView },
-  ],
+    { path: '/',               name: 'home',        component: HomeView },
+    { path: '/checkin',        name: 'checkin',     component: CheckinView },
+    { path: '/my-checkins',    name: 'mycheckins',  component: MyCheckinsView },
+    { path: '/resources',      name: 'resources',   component: ResourcesView },
+    { path: '/signin',         name: 'signin',      component: SignInView },
+    { path: '/:pathMatch(.*)*', name: 'notfound',   component: HomeView }
+  ]
 })
 
 export default router
