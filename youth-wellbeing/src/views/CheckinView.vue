d0617affa8cf5d3b9ca4b672d97789673399e566
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:16:45 +1100

    adding moodcheck tab to the navbar

---
+++ youth-wellbeing/src/views/CheckinView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref as vref } from 'vue'
+import { db, auth } from '@/lib/firebase'
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+const mood = vref(3); const note = vref(''); const uid = vref<string|null>(null)
+const router = useRouter()
+onAuthStateChanged(auth, (u)=> uid.value = u?.uid ?? null)
+
