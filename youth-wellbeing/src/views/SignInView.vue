4e91ed685a86eaa0aa8e3bdaa1a2d9413f889d60
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:11:56 +1100

    adding down the template for sign in + register

--- youth-wellbeing/src/views/SignInView.vue
+++ youth-wellbeing/src/views/SignInView.vue
@@ -15,3 +15,15 @@ const register = async () => {
 </script>
 
 
+<template>
+  <div class="container py-4" style="max-width:480px">
+    <h2>Sign in / Register</h2>
+    <input class="form-control my-2" v-model="email" placeholder="Email" />
+    <input class="form-control my-2" v-model="password" type="password" placeholder="Password" />
+    <div class="d-flex gap-2">
+      <button class="btn btn-primary" @click="signIn">Sign in</button>
+      <button class="btn btn-outline-secondary" @click="register">Register</button>
+    </div>
+    <p v-if="error" class="text-danger mt-2">{{ error }}</p>
+  </div>
+</template>
