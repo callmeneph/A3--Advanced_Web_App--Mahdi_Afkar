9372d7c93b132f22889c1c4247d08555a869f2fc
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-20 13:10:53 +1100

    sign in and register functions of the signin tab

---
+++ youth-wellbeing/src/views/SignInView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '@/lib/firebase'
+const email = ref(''); const password = ref(''); const error = ref('')
+
+const signIn = async () => {
+  try { await signInWithEmailAndPassword(auth, email.value, password.value) }
+  catch (e) { error.value = e.message }
+}
+const register = async () => {
+  try { await createUserWithEmailAndPassword(auth, email.value, password.value) }
+  catch (e) { error.value = e.message }
+}
+</script>
+
+
